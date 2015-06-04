e25ef9557d5ed04624eb409f6b2c21962619d60d
Junichi Ito <jit@sonicgarden.jp>
2015-06-04 21:42:23 +0900

    Refactoring.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,8 +4,8 @@ feature 'Home' do
   scenario 'select change', js: true do
     visit root_path
     expect(page).to_not have_content 'changed!'
-    expect(page).to have_select 'page_name'
-    select 'Railsの基礎', from: 'page_name'
+    expect(page).to have_select 'select_field'
+    select 'Railsの基礎', from: 'select_field'
     expect(page).to have_content 'changed!'
   end
 end
